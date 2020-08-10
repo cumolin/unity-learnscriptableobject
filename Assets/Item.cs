08843193ec70c5cb095a21cf1925c4c8488dac5e
cumo <cumo24386433@gmail.com>
2020-08-10 16:39:06 +0800

    this is a unity project to learn scriptableobject

---
+++ Assets/Item.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Create Item")]
+public class Item : ScriptableObject
+{
+    public string name;
+    public int id;
+    public string description;
+
+    public Buff buff;
+}
+
+public enum Attributes
+{
+    TypeA,
+    TypeB,
+    TypeC
+}
+
+[System.Serializable]
+public class Buff
+{
+    public Attributes attribute;
+    public int value;
+}
+
+
---
+++ Assets/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public Item item;
+
+    private void Start()
+    {
+        Debug.Log(item.name);
+        Debug.Log(item.id);
+        Debug.Log(item.buff.attribute + " = " + item.buff.value);
+    }
+}
